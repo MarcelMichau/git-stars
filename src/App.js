fbadc4586587e188c5b12a4796b5d3d3a0e1ab14
Marcel Michau <marcelmichau@gmail.com>
2017-05-21 11:50:41 +0200

    Switched react-router BrowserRouter to HashRouter for GitHub Pages support

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import AppNavBar from './components/AppNavBar';
 import StarsContainer from './components/StarsContainer';
 import LazyComponent from './components/LazyComponent';
