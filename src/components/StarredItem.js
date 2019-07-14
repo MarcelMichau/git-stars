3a49badd487c9922129835fd5394cfde3176229e
Marcel Michau <marcelmichau@gmail.com>
2019-07-14 10:45:24 +0200

    chore(deps): bump dependencies change repo description propType to not required

--- src/components/StarredItem.js
+++ src/components/StarredItem.js
@@ -6,43 +6,43 @@ import { Card, Icon, Image } from 'semantic-ui-react';
 const StarredItem = ({
 	repo: { starred_at: starredDate, repo: repoContent }
 }) => (
-	<Card>
-		<Card.Content>
-			<Card.Header>
-				<a
-					href={repoContent.html_url}
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					{repoContent.full_name}
-				</a>
-			</Card.Header>
-		</Card.Content>
-		<Image src={repoContent.owner.avatar_url} />
-		<Card.Content>
-			<span className="right floated">
-				<i className="star icon" aria-hidden="true" />
-				<span>{repoContent.stargazers_count}</span>
-			</span>
-			<Card.Meta>
-				<span className="date">
-					Starred on: <NiceDateify date={starredDate} />
+		<Card>
+			<Card.Content>
+				<Card.Header>
+					<a
+						href={repoContent.html_url}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						{repoContent.full_name}
+					</a>
+				</Card.Header>
+			</Card.Content>
+			<Image src={repoContent.owner.avatar_url} />
+			<Card.Content>
+				<span className="right floated">
+					<i className="star icon" aria-hidden="true" />
+					<span>{repoContent.stargazers_count}</span>
 				</span>
-			</Card.Meta>
-			<Card.Description>{repoContent.description}</Card.Description>
-		</Card.Content>
-		<Card.Content extra>
-			<span>
-				<Icon name="code" />
-				{repoContent.language ? (
-					<span>{repoContent.language}</span>
-				) : (
-					<span>Unknown</span>
-				)}
-			</span>
-		</Card.Content>
-	</Card>
-);
+				<Card.Meta>
+					<span className="date">
+						Starred on: <NiceDateify date={starredDate} />
+					</span>
+				</Card.Meta>
+				<Card.Description>{repoContent.description}</Card.Description>
+			</Card.Content>
+			<Card.Content extra>
+				<span>
+					<Icon name="code" />
+					{repoContent.language ? (
+						<span>{repoContent.language}</span>
+					) : (
+							<span>Unknown</span>
+						)}
+				</span>
+			</Card.Content>
+		</Card>
+	);
 
 StarredItem.propTypes = {
 	repo: PropTypes.shape({
@@ -51,7 +51,7 @@ StarredItem.propTypes = {
 			html_url: PropTypes.string.isRequired,
 			full_name: PropTypes.string.isRequired,
 			stargazers_count: PropTypes.number.isRequired,
-			description: PropTypes.string.isRequired,
+			description: PropTypes.string,
 			language: PropTypes.string,
 			owner: PropTypes.shape({
 				avatar_url: PropTypes.string.isRequired
