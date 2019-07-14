e3971a65e17f4f647320734446c0d6b2b93077bb
Marcel Michau <marcelmichau@gmail.com>
2019-07-14 10:48:21 +0200

    Updates

---
+++ precache-manifest.8abaa233b8e4d980ec23d13eed1194a6.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50f254afca73d1ee057f22ca40cc2f98",
+    "url": "/git-stars/index.html"
+  },
+  {
+    "revision": "915f58b55463d7de8f55",
+    "url": "/git-stars/static/css/2.f39c207c.chunk.css"
+  },
+  {
+    "revision": "915f58b55463d7de8f55",
+    "url": "/git-stars/static/js/2.33420845.chunk.js"
+  },
+  {
+    "revision": "33df613259bea6717457",
+    "url": "/git-stars/static/js/3.866cd774.chunk.js"
+  },
+  {
+    "revision": "7f81a5bb0f147a6e4cf8",
+    "url": "/git-stars/static/js/4.6fe5b534.chunk.js"
+  },
+  {
+    "revision": "86c9d619b929a74d6097",
+    "url": "/git-stars/static/js/5.024bab22.chunk.js"
+  },
+  {
+    "revision": "fc24334faf025564c019",
+    "url": "/git-stars/static/js/main.e44088b9.chunk.js"
+  },
+  {
+    "revision": "777138f3c19775d6fd14",
+    "url": "/git-stars/static/js/runtime~main.cafc8570.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/git-stars/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/git-stars/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/git-stars/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/git-stars/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/git-stars/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/git-stars/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/git-stars/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/git-stars/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/git-stars/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/git-stars/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/git-stars/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/git-stars/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/git-stars/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/git-stars/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/git-stars/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/git-stars/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
